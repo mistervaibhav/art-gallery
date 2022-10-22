161e596f8ad16a8fbe1a945e59ee4e8864c8565b
mistervaibhav <singhvaibhavofficial@gmail.com>
2022-10-23 02:26:07 +0530

    Config Changes

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/css-arts' ,
-  assetPrefix: isProd ? "/css-arts/" : "",
+  assetPrefix: "",
   reactStrictMode: true,
   swcMinify: true,
   images: {
