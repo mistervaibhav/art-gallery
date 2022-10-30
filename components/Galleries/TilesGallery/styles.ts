fefb85bf786ebe0cf0b893a535f5d63ad7a788a1
mistervaibhav <singhvaibhavofficial@gmail.com>
2022-10-30 18:13:29 +0530

    Started working on a simple gallery

---
+++ components/Galleries/TilesGallery/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  border: 2px dashed red;
+`;
+
+export const Tile = styled.a`
+  border: 2px dashed green;
+`;
+
+export const Iframe = styled.iframe`
+  border: 2px dashed blue;
+  pointer-events: painted;
+`;
